6afd51c0f025c05c35575b78af1ae7ddd9e59f76
Yunus Yazici <55270091+yunuseminyazici@users.noreply.github.com>
2023-01-18 22:48:25 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 from flask import Flask, render_template, request
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+@app.route('/mapping_suggestion', methods=['GET', 'POST'])
+def mapping_suggestion():
+    suggestion = [{'name':'datapoint1', 'node':'https://www.examle.com/datapoint1'},{'name':'datapoint2', 'node':'https://www.examle.com/datapoint2'}]
+    return render_template('mapping_suggestion.html', suggestion=suggestion)    
+
 @app.route('/mappings', methods=['GET', 'POST'])
 def mappings():
-    suggestion = ['book1', 'book2', 'book3']
+    name = request.form.getlist('name')
+    node = request.form.getlist('node')
+    selected_mappings = list(zip(name, node))
+    response_data = [{'name': name, 'node': node} for name, node in selected_mappings]
+    #json_response = json.dumps(response_data)
     colours = ['red', 'blue', 'green']
-    return render_template('mapping.html', suggestion=suggestion, colours=colours)
+    return render_template('mapping.html', suggestion=response_data, colours=colours)
 
 @app.route('/mapping_suggestion_accept', methods=['POST'])
 def mapping_suggestion_accept():
